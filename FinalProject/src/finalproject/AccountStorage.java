5c66ddff8ed7db1322a526dbb7696a1f87f4d566
yodacatedeu <47410714+yodacatedeu@users.noreply.github.com>
2019-05-28 14:43:28 -0700

    Add files via upload

--- FinalProject/src/finalproject/AccountStorage.java
+++ FinalProject/src/finalproject/AccountStorage.java
@@ -23,44 +23,82 @@ public class AccountStorage {
         pinsAndNames = new HashMap();
     }
     
-    private void sort() {
+    private void sort(int low, int high) {
         // implement some kind of sort tht sorts accounts in alphabetical order based off names
-        // bubble sort
-        for (int i = 0; i < accounts.size() - 1; i++) {
-            for (int j = 0; j < accounts.size() - i - 1; j++) {
-                 if (accounts.get(j).getFullName().compareTo(accounts.get(j + 1).getFullName()) > 0) {
-                    Account temp = accounts.get(j);
-                    accounts.set(j, accounts.get(j + 1));
-                    accounts.set(j + 1, temp);
-                }
-            }
-        }
+        
+       // bubble sort for initial binary search test
+//        for (int i = 0; i < accounts.size() - 1; i++) {
+//            for (int j = 0; j < accounts.size() - i - 1; j++) {
+//                 if (accounts.get(j).getFullName().compareTo(accounts.get(j + 1).getFullName()) > 0) {
+//                    Account temp = accounts.get(j);
+//                    accounts.set(j, accounts.get(j + 1));
+//                    accounts.set(j + 1, temp);
+//                }
+//            }
+//        }
+
+        // Quick Sort
+         if (low < high) { 
+            /* pI is partitioning index, arr[pI] is  
+              now at right place */
+            int pI = partition(low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(low, pI - 1); 
+            sort(pI + 1, high); 
+        } 
+
         sorted = true;
     }
     
+    private int partition(int low, int high) { 
+        String pivot = accounts.get(high).getFullName();  
+        int i = (low - 1); // index of smaller element 
+        for (int j = low; j < high; j++) { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (accounts.get(j).getFullName().compareToIgnoreCase(pivot) <= 0) { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                Account temp = accounts.get(i);  
+                accounts.set(i, accounts.get(j));
+                accounts.set(j, temp);
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        Account temp = accounts.get(i + 1); 
+        accounts.set(i + 1, accounts.get(high));
+        accounts.set(high, temp);
+  
+        return i + 1; 
+    } 
+    
     private Account search(String name, int low, int high) {
-        if (!sorted) {
+        if (!sorted) { // check if sorted before binary search
             //System.out.println("Sorting...");
-            sort();
+            sort(low, high);
             //System.out.println("sorted");
         }    
-        // binary search
+        // Binary Search
         // Base Case
         if (low > high) {
             return null;
         } else {
             int mid = (low + high) / 2;
-            if (name.equals(accounts.get(mid).getFullName())) {
+            if (name.equalsIgnoreCase(accounts.get(mid).getFullName())) {
                 return accounts.get(mid);
-            } else if (name.compareTo(accounts.get(mid).getFullName()) > 0) {
-                return search(name, mid + 1, high);
+            } else if (name.compareToIgnoreCase(accounts.get(mid).getFullName()) > 0) {
                 // change the low to the midpoint
                 // keep everything the same
                 // and call search again
+                return search(name, mid + 1, high);
             } else {
                 // change the high to the midpoint
-                return search(name, low, mid - 1);
                 // call search again
+                return search(name, low, mid - 1);
             }
         }
     }
@@ -73,7 +111,7 @@ public class AccountStorage {
            //System.out.println("Name don't exist");
            return null;
        }
-       return search(name, 0 , accounts.size() -1);
+       return search(name, 0 , accounts.size() - 1);
     }
     
     public void add(Account accnt) {
@@ -87,14 +125,15 @@ public class AccountStorage {
         sorted = false;
     }
     
-    public void printAccnts() {
-        System.out.println("Map:");
-        System.out.println(pinsAndNames);
-        
-        System.out.println("ArrayList:");
-        for (int i = 0; i < accounts.size(); i++) {
-            System.out.println(accounts.get(i).getFullName());
-        }
-    }
+      // only for testing purposes
+//    public void printAccnts() { 
+//        System.out.println("Map:");
+//        System.out.println(pinsAndNames);
+//        
+//        System.out.println("ArrayList:");
+//        for (int i = 0; i < accounts.size(); i++) {
+//            System.out.println(accounts.get(i).getFullName());
+//        }
+//    }
      
 }
